4f2b35ea2ca3ad0af7fdc782455c32ffa5806a5c
Jonathan Callahan <jonathancallahan@Stuart.domain>
2020-11-12 12:59:49 -0800

    tweak

--- local_jon/state_map.R
+++ local_jon/state_map.R
@@ -8,27 +8,27 @@ mazama_initialize(spatialDataDir = "~/Data/Spatial")
 # ----- Get COVID data ---------------------------------------------------------
 
 # > names(stateCovid)
-#  [1] "id"                                  "date"                               
-#  [3] "tests"                               "confirmed"                          
-#  [5] "recovered"                           "deaths"                             
-#  [7] "hosp"                                "vent"                               
-#  [9] "icu"                                 "population"                         
-# [11] "school_closing"                      "workplace_closing"                  
-# [13] "cancel_events"                       "gatherings_restrictions"            
-# [15] "transport_closing"                   "stay_home_restrictions"             
+#  [1] "id"                                  "date"
+#  [3] "tests"                               "confirmed"
+#  [5] "recovered"                           "deaths"
+#  [7] "hosp"                                "vent"
+#  [9] "icu"                                 "population"
+# [11] "school_closing"                      "workplace_closing"
+# [13] "cancel_events"                       "gatherings_restrictions"
+# [15] "transport_closing"                   "stay_home_restrictions"
 # [17] "internal_movement_restrictions"      "international_movement_restrictions"
-# [19] "information_campaigns"               "testing_policy"                     
-# [21] "contact_tracing"                     "stringency_index"                   
-# [23] "iso_alpha_3"                         "iso_alpha_2"                        
-# [25] "iso_numeric"                         "currency"                           
-# [27] "administrative_area_level"           "administrative_area_level_1"        
-# [29] "administrative_area_level_2"         "administrative_area_level_3"        
-# [31] "latitude"                            "longitude"                          
-# [33] "key"                                 "key_google_mobility"                
-# [35] "key_apple_mobility"                  "key_alpha_2"                        
-# [37] "key_numeric"                        
+# [19] "information_campaigns"               "testing_policy"
+# [21] "contact_tracing"                     "stringency_index"
+# [23] "iso_alpha_3"                         "iso_alpha_2"
+# [25] "iso_numeric"                         "currency"
+# [27] "administrative_area_level"           "administrative_area_level_1"
+# [29] "administrative_area_level_2"         "administrative_area_level_3"
+# [31] "latitude"                            "longitude"
+# [33] "key"                                 "key_google_mobility"
+# [35] "key_apple_mobility"                  "key_alpha_2"
+# [37] "key_numeric"
 
-stateCovid <- 
+stateCovid <-
   COVID19::covid19("US", level = 2) %>%
   rename(
     countryCode = iso_alpha_2,
@@ -46,9 +46,9 @@ stateCovid <-
   )
 
 # Try out a single date
-covidDF <- 
+covidDF <-
   stateCovid %>%
-  dplyr::filter(date == "2020-10-10")
+  dplyr::filter(date == "2020-11-11")
 
 # ----- Create multi-parameter map ---------------------------------------------
 
@@ -60,14 +60,20 @@ MazamaSpatialPlots::stateMap(
     "hosp_per_100K",
     "deaths_per_100K"
   ),
-  title = "Overall Title"
+  title = c(
+    "Tests",
+    "Cases",
+    "Hospitalizations",
+    "Deaths"
+  ),
+  main.title = "A Grim Outlook"
 )
 
 
 
 # ----- Movie??? ---------------------------------------------------------------
 
-datestamps <- 
+datestamps <-
   MazamaCoreUtils::dateSequence(20200601, 20200731, timezone = "UTC") %>%
   MazamaCoreUtils::timeStamp(style = "clock", unit = "day", timezone = "UTC")
 
@@ -79,7 +85,7 @@ for ( datestamp in datestamps ) {
   timestep <- timestep + 1
   fileName <- sprintf("%s%03d.png", imageNameBase, timestep)
   filePath <- file.path(tempdir(), fileName)
-  
+
   tm <- MazamaSpatialPlots::stateMap(
     data = dplyr::filter(stateCovid, date == datestamp),
     parameter = c(
@@ -87,9 +93,9 @@ for ( datestamp in datestamps ) {
     ),
     title = datestamp
   )
-  
+
   ###print(gg)
-  
+
   # Save plot
   tmap::tmap_save(
     tm = tm,
@@ -100,9 +106,9 @@ for ( datestamp in datestamps ) {
     dpi = 72,
     scale = NA
   )
-  
+
   # Results in "PNG image data, 720 x 576, 8-bit/color RGBA, non-interlaced"
-  
+
 }
 
 # ----- Create video with ffmpeg -----------------------------------------------
@@ -116,9 +122,9 @@ outputDir <- path.expand(".")
 # Define system calls to ffmpeg to create video from frames
 cmd_cd <- paste0("cd ", tempdir())
 cmd_ffmpeg <- paste0(
-  "ffmpeg -y -loglevel quiet -r ", 
-  frameRate, " -f image2 -s 720x576 -i ", 
-  imageNameBase, "%03d.png -vcodec libx264 -crf 25 ", 
+  "ffmpeg -y -loglevel quiet -r ",
+  frameRate, " -f image2 -s 720x576 -i ",
+  imageNameBase, "%03d.png -vcodec libx264 -crf 25 ",
   # https://bugzilla.mozilla.org/show_bug.cgi?id=1368063#c7
   "-pix_fmt yuv420p ",
   outputDir, "/", fileName
